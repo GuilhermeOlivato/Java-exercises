474b2aaa37168948811f2ca87ac3f3f9558378ef
Guilherme Olivato <oolivato@outlook.com>
2026-04-01 20:38:04 -0300

    update

--- Division/src/main/java/com/division/HelloController.java
+++ Division/src/main/java/com/division/HelloController.java
@@ -1,14 +1,34 @@
 package com.division;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label divisao;
+    @FXML
+    private Label resto;
+    @FXML
+    private Button dividirBotao;
+    @FXML
+    private TextField valor1;
+    @FXML
+    private TextField valor2;
+    @FXML
+    private AnchorPane fundo;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void clicouBotao() {
+        int n1 = Integer.parseInt(valor1.getText());
+        int n2 = Integer.parseInt(valor2.getText());
+        float div = ((float)n1/n2);
+        float res = (n1%n2);
+
+        divisao.setText(Float.toString(div));
+        resto.setText(Float.toString(res));
+
     }
 }
