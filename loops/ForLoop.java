f1493eb72935dc3b24320f833005f612b6532795
Guilherme Olivato <oolivato@outlook.com>
2026-04-10 20:44:34 -0300

    Update ForLoop.java

--- loops/ForLoop.java
+++ loops/ForLoop.java
@@ -6,17 +6,24 @@ public class ForLoop {
         Scanner teclado = new Scanner(System.in);
         int valor;
         int soma = 0;
+        Scanner 
 
-         for (int c=0; c < 10; c++) {
+        System.out.println("Escolha quantas vezes o valor será inserido: ");
+        int vezes = teclado.nextInt(); 
+        
+        for (int c=0; c < vezes; c++) {
 
             System.out.print("Digite um valor: ");
             valor = teclado.nextInt();
             soma = soma + valor;
             System.out.println("Valor atual da soma: " + soma);
-
-         }
-
-
+        }
+         
+        
     }
+    
+    System.out.println("Foram somados " + vezes + " valores. Sendo a soma total: " + soma);
+        teclado.close();
+
 
 }
