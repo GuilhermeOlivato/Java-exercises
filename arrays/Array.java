0b664aee6dbc90ae11279f069c6e1b0f21f3a868
Guilherme Olivato <oolivato@outlook.com>
2026-04-14 19:25:01 -0300

    update

--- arrays/Array.java
+++ arrays/Array.java
@@ -3,22 +3,27 @@ public class Array {
     public static void main(String[] args) {
         int[] fruits = {};
         String[] names = {};
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Digite o numero de frutas: ");
-        int NumFruits = teclado.nextInt();
-        fruits = new int[NumFruits];
-        names = new String[NumFruits];
-        for (int i = 0; i < fruits.length; i++) {
-            System.out.println("Digite o nome da fruta: ");
-            String name = teclado.next();
-            names[i] = name;
-            System.out.println("Digite a quantidade de " + name + ": ");
-            int quantity = teclado.nextInt();
-            fruits[i] = quantity;
-        }   
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o numero de frutas: ");
+            int NumFruits = scanner.nextInt();
+            fruits = new int[NumFruits];
+            names = new String[NumFruits];
+            
+            
+            // Lendo os nomes e quantidades das frutas
+            for (int i = 0; i < fruits.length; i++) {
+                System.out.println("Digite o nome da fruta: ");
+                String name = scanner.next();
+                names[i] = name;
+                System.out.println("Digite a quantidade de " + name + ": ");
+                int quantity = scanner.nextInt();
+                fruits[i] = quantity;
+            }
+        }
+            // Exibindo as frutas e suas quantidades
         System.out.println("Frutas e suas quantidades: ");
         for (int i = 0; i < fruits.length; i++) {
-            System.out.println(names[i] + " " + (i + 1) + ": " + fruits[i]);
+            System.out.println(names[i] + ": " + fruits[i]);
         }
     }
 
