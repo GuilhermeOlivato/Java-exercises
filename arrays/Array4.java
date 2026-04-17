d820643e82ea7072fd1f6487f646a0e3824d8d57
Guilherme Olivato <oolivato@outlook.com>
2026-04-16 23:08:18 -0300

    Update Array4.java

--- arrays/Array4.java
+++ arrays/Array4.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Array4 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] foods;
+        int size;
+
+        System.out.print("How many foods do you want to select?");
+
+        size = scanner.nextInt();
+        scanner.nextLine();
+        foods = new String[size];
+        
+        for(int i = 0; i < foods.length; i++ ){
+            System.out.println("Type a food: ");
+            foods[i] = scanner.nextLine();
+        
+        
+        }
+        
+        
+        for (String food : foods){
+            System.out.println(food);
+        }
+    }
 
 }
