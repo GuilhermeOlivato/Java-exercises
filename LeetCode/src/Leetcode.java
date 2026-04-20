3b2f2533afaf827095ce1e277b74d17a756b159e
Guilherme Olivato <oolivato@outlook.com>
2026-04-20 19:38:16 -0300

    update

---
+++ LeetCode/src/Leetcode.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Scanner teclado = new Scanner(System.in);
+        int nums = [];
+        int target;
+
+
+        for(int i=0; i)
+
+
+    }
+}
