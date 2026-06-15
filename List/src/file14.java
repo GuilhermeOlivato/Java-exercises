1ff724471c6437f3bd97030bdde9b3dc2267b0a0
Guilherme Olivato <oolivato@outlook.com>
2026-06-14 21:11:57 -0300

    update

---
+++ List/src/file14.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class file14 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int travel = 0;
+        System.out.println("How many Km will you travel? ");
+        travel = teclado.nextInt();
+
+        System.out.println(travel);
+        
+    }
+}
