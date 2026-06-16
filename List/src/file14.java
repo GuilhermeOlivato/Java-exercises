31fd2838a91ddab7da50b8692192e93020563daa
Guilherme Olivato <oolivato@outlook.com>
2026-06-15 23:55:54 -0300

    update

--- List/src/file14.java
+++ List/src/file14.java
@@ -8,6 +8,16 @@ public class file14 {
         System.out.println("How many Km will you travel? ");
         travel = teclado.nextInt();
 
+        double fuel = travel * 0,2;
+        
+        int days = 0;
+
+        System.out.println("For how many days will you rent the car?");
+        days = teclado.nextInt();
+
+        double priceDay = days * 90,00;
+        
+
         System.out.println(travel);
         
     }
