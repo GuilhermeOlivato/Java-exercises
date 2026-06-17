0171313fcf02b61c79e8367cadac75c810e01abc
Guilherme Olivato <oolivato@outlook.com>
2026-06-17 15:27:24 -0300

    update

--- List/src/file14.java
+++ List/src/file14.java
@@ -4,21 +4,24 @@ public class file14 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int travel = 0;
+        double travel = 0;
         System.out.println("How many Km will you travel? ");
-        travel = teclado.nextInt();
+        travel = teclado.nextDouble();
 
-        double fuel = travel * 0,2;
         
-        int days = 0;
-
+        double days = 0;
+        
         System.out.println("For how many days will you rent the car?");
-        days = teclado.nextInt();
-
-        double priceDay = days * 90,00;
+        days = teclado.nextDouble();
+        
+        double priceDay = days * 90;
+        double fuelPrice = (travel * 20)/100;
         
 
-        System.out.println(travel);
+        System.out.println((travel) + " - " + (priceDay) + " - " + (days) + " - " + (fuelPrice));
+        System.out.println(fuelPrice);
+        
+
         
     }
 }
