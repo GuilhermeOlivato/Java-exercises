e7b3aebb60461e8853cc03f7ea4a7649f8a4354e
Guilherme Olivato <oolivato@outlook.com>
2026-06-21 23:38:55 -0300

    Create file12.java

---
+++ List/src/file12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class file12 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double price = 0;
+        System.out.println("Type the price of the product: ");
+        price = teclado.nextDouble();
+
+        double finalPrice = (price * )
+
+    }
+}
