312c09662d6e67804df32fd5b22f3e8a7e81b04c
Guilherme Olivato <oolivato@outlook.com>
2026-06-26 20:07:30 -0300

    Create file8.java

---
+++ List/src/file8.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class file8 {
+    public static void main(String[] args) {
+        double meters;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Type the quantity of meters: ");
+        meters = teclado.nextDouble();
+
+        double kilometer = meters/1000;
+        double hectometer = meters/100;
+        double decameter = meters/10;
+        double decimeter = meters*10;
+        double centimeter = meters*100;
+        double millimeter = meters*1000;
+
+        System.out.println(meters + "meters is the equivalent to: ");
+
+        System.out.println(kilometer + " Kilometers.");
+        System.out.println("------------------");
+        System.out.println(hectometer + "Hectometers.");
+        System.out.println("------------------");
+        System.out.println(decameter + "Decameters.");
+        System.out.println("------------------");
+        System.out.println(decimeter + "Decimeters.");
+        System.out.println("------------------");
+        System.out.println(centimeter + "Centimeters.");
+        System.out.println("------------------");
+        System.out.println(millimeter + "Millimeters.");
+        System.out.println("------------------");
+
+    }
+}
