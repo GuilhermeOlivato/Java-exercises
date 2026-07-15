dc585ba185d2ed52b94f369cae0c88e8edaa00e2
Guilherme Olivato <oolivato@outlook.com>
2026-07-15 00:28:54 -0300

    daily practice

---
+++ List/src/file18.java
@@ -0,0 +1,24 @@
+package List.src;
+import java.util.Scanner;
+import java.time.LocalDate;
+
+
+public class file18 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int age;
+        int date;
+        int currentDate = 2026;
+        System.out.println("Type your date of birth:");
+        date = teclado.nextInt();
+        age = (currentDate - date);
+        if (age>60 || (age>=16 && age<18)){
+            System.out.println("You can vote if You want.");
+
+        } else if (age>=18 && age<60) {
+            System.out.println("You have to vote.");
+        }   else if (age<16) {
+                System.out.println("You can't vote.");
+        }
+    }
+}
